652371de5bbe5cd1e2738a26534b5778edeae1c4
Yan Soares Couto <yancouto@fb.com>
2020-06-02 21:09:54 +0100

    Small struct builder refactor
    Will use this code for enum as well

--- rlua-builders-derive/src/lib.rs
+++ rlua-builders-derive/src/lib.rs
@@ -4,43 +4,54 @@ use proc_macro2::{TokenStream as TokenStream2};
 use quote::quote;
 
 
-fn from_unit(name: Ident) -> TokenStream2 {
-    quote! {
-        impl<'s> LuaStructBuilder<'s, Self> for #name {
-            fn builder(ctx: rlua::Context<'s>) -> rlua::Result<Self> {
-                Ok(Self)
-            }
-        }
-    }
-}
 
-fn from_unnamed(name: Ident, fields: FieldsUnnamed) -> TokenStream2 {
+fn builder_for_unnamed(name: TokenStream2, fields: FieldsUnnamed) -> TokenStream2 {
     let i = (0..fields.unnamed.len()).map(Index::from);
     quote! {
-        impl<'s> LuaStructBuilder<'s, rlua::Function<'s>> for #name {
-            fn builder(ctx: rlua::Context<'s>) -> rlua::Result<rlua::Function<'s>> {
-                ctx.create_function(|_, args: #fields| {
-                    Ok(Self(#(args.#i,)*))
-                })
-            }
-        }
+        ctx.create_function(|_, args: #fields| {
+            Ok(#name (#(args.#i,)*))
+        })
     }
 }
 
-fn from_named(name: Ident, fields : FieldsNamed) -> TokenStream2 {
+fn builder_for_named(name: TokenStream2, fields : FieldsNamed) -> TokenStream2 {
     let names = fields.named.iter().map(|x| x.ident.clone());
     let types = fields.named.iter().map(|x| x.ty.clone());
 
     quote! {
+        ctx.create_function(|_, data: rlua::Table<'s>| {
+            Ok(#name {
+                #(
+                    #names: data.get::<_, #types>(stringify!(#names))?,
+                )*
+            })
+        })
+    }
+}
+
+fn builder_for_fields(name: TokenStream2, fields: Fields) -> TokenStream2 {
+    match fields {
+        Fields::Unit => name,
+        Fields::Unnamed(unnamed) => builder_for_unnamed(name, unnamed),
+        Fields::Named(named) => builder_for_named(name, named),
+    }
+}
+
+fn function_struct_builder(name: Ident, builder: TokenStream2) -> TokenStream2 {
+    quote! {
         impl<'s> LuaStructBuilder<'s, rlua::Function<'s>> for #name {
             fn builder(ctx: rlua::Context<'s>) -> rlua::Result<rlua::Function<'s>> {
-                ctx.create_function(|_, data: rlua::Table<'s>| {
-                    Ok(Self {
-                        #(
-                            #names: data.get::<_, #types>(stringify!(#names))?,
-                        )*
-                    })
-                })
+                #builder
+            }
+        }
+    }
+}
+
+fn self_struct_builder(name: Ident, builder: TokenStream2) -> TokenStream2 {
+    quote! {
+        impl<'s> LuaStructBuilder<'s, Self> for #name {
+            fn builder(ctx: rlua::Context<'s>) -> rlua::Result<Self> {
+                Ok(#builder)
             }
         }
     }
@@ -56,10 +67,11 @@ pub fn derive_struct_builder(input: TokenStream) -> TokenStream {
         _ => panic!("Must annotate struct"),
     };
 
+    let code = builder_for_fields(quote!{Self}, ds.fields.clone());
+
     let code = match ds.fields {
-        Fields::Unit => from_unit(name),
-        Fields::Unnamed(unnamed) => from_unnamed(name, unnamed),
-        Fields::Named(named) => from_named(name, named),
+        Fields::Unit => self_struct_builder(name, code),
+        Fields::Unnamed(..) | Fields::Named(..) => function_struct_builder(name, code),
     };
 
     TokenStream::from(code)
